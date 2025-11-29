99859e4b56a4b629a27589f5c5711a1e14326e4b
Nicholas Chai <nicholas.chai@intelligis.io>
2025-11-29 21:53:10 +0800

    additional stubbing into the erase operation added todo jobs and added the elapse time for the process

--- server/app/api/v1/operations/erase.py
+++ server/app/api/v1/operations/erase.py
@@ -27,12 +27,39 @@ def erase_operation(
 
     logger.info(f"Task {self.request.id}: Starting erase_task")
 
-    result_msg = {
-        "message": "Target erased successfully",
-        "output_type": "data",
-        "output_filepath": None,
-        "output_data": "{'message':'one day this will be data'}",
-        "processing_time_seconds": 0,
-    }
+    try:
+        start_time = datetime.datetime.now()
+        self.update_state(state="STARTED", meta={"message": "Erase task started"})
 
-    return result_msg
+        # read input data files (target and erase) + turn them into gdf
+        self.update_state(state="PROCESSING", meta={"message": "Reading data"})
+        # TODO: read the files
+
+        # apply the erase
+        self.update_state(state="PROCESSING", meta={"message": "Applying erase"})
+        # TODO: implement and call erase geoprocessing function
+
+        # write to desired output format
+        self.update_state(state="PROCESSING", meta={"message": "Writing output"})
+        # TODO: write the resulting erased gdf to output file
+
+        end_time = datetime.datetime.now()
+        elapsed = (end_time - start_time).total_seconds()
+
+        result_msg = {
+            "message": "Target erased successfully",
+            "output_type": "data",
+            "output_filepath": None,
+            "output_data": "{'message':'one day this will be data'}",
+            "processing_time_seconds": elapsed,
+        }
+
+        return result_msg
+    except Exception as e:
+        logger.error(
+            f"Task {self.request.id}: Failed erase task with error: {e}", exc_info=True
+        )
+        self.update_state(
+            state="FAILURE", meta={"exc_type": type(e).__name__, "exc_message": str(e)}
+        )
+        raise
